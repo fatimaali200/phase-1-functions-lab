5af9fc929a459c79013b42c03e2de8009228761d
Fatima Ali <fofoalmali@gmail.com>
2023-03-20 23:41:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue){
+
+    let numberOfBlock;
+    if(someValue > 42){
+       numberOfBlock=someValue-42;
+    }
+    else{
+        numberOfBlock = 42 -someValue;
+    }
+    return numberOfBlock;
+}
+
+function distanceFromHqInFeet(someValue){
+
+  let blocks=distanceFromHqInBlocks(someValue)
+  return blocks*264;
+}
+
+function distanceTravelledInFeet(start, destination){
+  if(destination > start){
+    return (destination-start)*264;
+  }
+  else if(destination < start){
+    return (start-destination)*264
+  }
+}
+
+function calculatesFarePrice(start, destination){
+
+    let feetNumber=distanceTravelledInFeet(start,destination);
+    if(feetNumber < 400){
+      return 0;
+    }
+    else if(feetNumber >400 && feetNumber<2000){
+       return 0.02*(feetNumber-400);
+       
+    }
+    else if(feetNumber > 2000 && feetNumber <2500){
+        return 25;
+    }
+    else {
+        return'cannot travel that far';
+    }
+}
